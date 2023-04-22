834a8bff5835e62eb12b2470f6e6e2a3c9bcecca
aliamerj <aliamer19ali@gmail.com>
2023-04-22 22:04:11 +0300

    fix comment bug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates :body, presence: true,
-                   length: { maximum: 500, minimum: 5 }
+                   length: { maximum: 500, minimum: 1 }
 end
